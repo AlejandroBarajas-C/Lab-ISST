d06c508f505c3caea4122909630d0619e28e59a8
AlejandroBarajas <alejandro.barajas.carracedo@alumnos.upm.es>
2026-04-20 17:00:30 +0200

    Remove unused import statements from ProfesorRepository

--- lab_datos/src/main/java/es/upm/dit/isst/lab_datos/repository/ProfesorRepository.java
+++ lab_datos/src/main/java/es/upm/dit/isst/lab_datos/repository/ProfesorRepository.java
@@ -1,13 +1,12 @@
 package es.upm.dit.isst.lab_datos.repository;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 import es.upm.dit.isst.lab_datos.domain.Profesor;
 
